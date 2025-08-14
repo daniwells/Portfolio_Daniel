d5421f3a54e5787a3822f678d2e38feba774478e
daniel <daniellimafreita@gmail.com>
2025-08-14 13:53:56 -0300

    :bug: fix: Adding logs to debug the email api

--- api/email.ts
+++ api/email.ts
@@ -2,21 +2,28 @@ import { Resend } from "resend";
 import PurchaseReceiptEmail from "../src/email/PurchaseReceiptEmail";
 
 export default async function handler(req: any, res: any) {
-  if (req.method !== "POST") return res.status(405).end();
+    if (req.method !== "POST") return res.status(405).end();
+    console.log("Recebido request:", req.body);
 
-  const { message, from, name } = req.body;
-  const resend = new Resend(process.env.RESEND_API_KEY);
+    const { message, from, name } = req.body;
+    console.log(message)
+    console.log(from)
+    console.log(name)
+    const resend = new Resend(process.env.RESEND_API_KEY);
+    console.log(resend)
 
-  try {
-    const data = await resend.emails.send({
-      from: `${from}`,
-      to: process.env.RECIPIENT_EMAIL!,
-      subject: `New Contact ${name} - Daniel's Portfolio`,
-      react: PurchaseReceiptEmail({ message }),
-    });
-
-    res.status(200).json({ success: true, data });
-  } catch (error) {
-    res.status(500).json({ error });
-  }
+    try {
+        console.log("PPPPPPPPPPPPPPPPPPPPPPPPPPPPPP")
+        const data = await resend.emails.send({
+            from: `${from}`,
+            to: process.env.RECIPIENT_EMAIL!,
+            subject: `New Contact ${name} - Daniel's Portfolio`,
+            react: PurchaseReceiptEmail({ message }),
+        });
+        console.log(data)
+        res.status(200).json({ success: true, data });
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({ error });
+    }
 }
