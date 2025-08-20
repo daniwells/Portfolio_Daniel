aa293382dbab86b4b9bdecbd2e936278739e0ff7
daniel <daniellimafreita@gmail.com>
2025-08-19 21:35:32 -0300

    feat: Fixing problems and developing popup to return the email response

---
+++ src/components/Popup.tsx
@@ -0,0 +1,72 @@
+import { MdMarkEmailRead, MdMarkEmailUnread } from "react-icons/md";
+import { XIcon } from "lucide-react";
+
+const Popup: React.FC<{
+    isOpen: boolean;
+    setIsOpen: (isOpen: boolean) => void;
+    username: string;
+    status: "error" | "success";
+}> = ({
+    isOpen, 
+    setIsOpen, 
+    username,
+    status
+}) => {
+    
+    if(!isOpen) return
+
+    return <div 
+            className="fixed z-50 flex justify-center items-center w-full inset-0 max-h-full bg-[rgba(0,0,0,0.4)] "
+        >
+        <div className="relative p-4 w-full sm:w-[30rem] max-w-2xl max-h-full">
+            <div className="relative bg-white rounded-lg shadow-sm">
+                <div className="flex items-center justify-between p-4 mb-4 border-b border-gray-300">
+                    <h3 className="text-xl font-semibold text-gray-900">
+                        Contact Me
+                    </h3>
+                    <button onClick={() => setIsOpen(false)} className="cursor-pointer text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center">
+                        <XIcon/>
+                    </button>
+                </div>
+                
+                <div className="p-4 text-balance text-gray-500 text-center w-full">
+                    {
+                        status === "success" ?
+                            <MdMarkEmailRead className="text-green-500 w-18 h-18 mx-auto"/>
+                        :
+                            <MdMarkEmailUnread className="text-red-500 w-18 h-18 mx-auto" />
+                    }
+
+                    {
+                        status === "success" ?
+                            <>
+                                <p>
+                                    Email was sent successfully!
+                                </p>
+                                <p><strong>{username}</strong>, Thanks for your contact.</p>
+                            </>
+                        :
+                            <>
+                                <p>
+                                    Sorry, there was an error sending the email!
+                                </p>
+                                <p>Please, try again later.</p>
+                            </>
+                    }
+                    
+                    
+                </div>
+                
+                <div className="flex items-center p-4">
+                    <button 
+                        onClick={() => setIsOpen(false)}
+                        className="cursor-pointer flex flex-wrap items-center gap-2 rounded-xl px-6 py-3 font-medium bg-blue-custom transition-all duration-200 hover:bg-blue-custom/80 group">
+                            Ok
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
+}
+ 
+export default Popup;
--- src/components/Portifolio.tsx
+++ src/components/Portifolio.tsx
@@ -18,7 +18,7 @@ const projects = [
   {
     title: "Ritmo da Forma",
     description: "An interactive and educational web game that simulates real-life financial situations",
-    builtWith: [<SiReact size={20} />, <SiDocker size={20}/>],
+    builtWith: [<SiReact key={1} size={20} />, <SiDocker key={11} size={20}/>],
     img: ritmoDaFormaImage,
     liveLink: "https://ritmodaforma.vercel.app/",
     githubLink: "https://github.com/ruansilva123/RitmoDaForma_PI.git",
@@ -27,7 +27,7 @@ const projects = [
   {
     title: "Arquiteto do Brownie",
     description: "A powerful and intuitive API for managing notes efficiently",
-    builtWith: [<SiNextdotjs size={20} />, <SiPostgresql size={20} />, <SiCloudinary size={20}/>],
+    builtWith: [<SiNextdotjs key={2} size={20} />, <SiPostgresql key={22} size={20} />, <SiCloudinary key={222} size={20}/>],
     img: arquitetoBrownieImage,
     liveLink: "https://arquitetodobrownie.vercel.app/",
     githubLink: "https://github.com/daniwells/Prostore_Course.git",
@@ -36,7 +36,7 @@ const projects = [
   {
     title: "Prostore",
     description: "Modern personal portfolio with animations",
-    builtWith: [<SiNextdotjs size={20} />, <SiPostgresql size={20} />, <SiJsonwebtokens size={20} />, <SiPaypal size={20}/>, <SiStripe size={20}/>],
+    builtWith: [<SiNextdotjs key={3} size={20} />, <SiPostgresql key={33} size={20} />, <SiJsonwebtokens key={333} size={20} />, <SiPaypal key={3333} size={20}/>, <SiStripe key={33333} size={20}/>],
     img: prostoreImage,
     liveLink: "https://prostore-course-three.vercel.app/",
     githubLink: "https://github.com/daniwells/Prostore_Course.git",
@@ -51,34 +51,34 @@ const Portifolio: React.FC = () => (
       <FolderGit2 className="h-4 w-4"/> Some of my recent work
     </div>
     <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-3">
-      {projects.map((p) => (
+      {projects.map((project) => (
         <div
-          key={p.title}
-          className={`p-4 group flex flex-col overflow-hidden rounded-2xl ${p.bgColor} shadow-[0_0_25px_rgba(255,255,255,0.05)]`}
+          key={project.title}
+          className={`p-4 group flex flex-col overflow-hidden rounded-2xl ${project.bgColor} shadow-[0_0_25px_rgba(255,255,255,0.05)]`}
         >
           <div className="relative h-40 w-full overflow-hidden">
             <div className="flex flex-col gap-2 items-center absolute top-1/2 left-1/2 transition duration-500 transform -translate-1/2 z-10 opacity-0 group-hover:opacity-100" >
-                <a href={p.liveLink} target="_blank" className="gap-2 flex items-center cursor-pointer hover:text-blue-custom">
+                <a href={project.liveLink} target="_blank" className="gap-2 flex items-center cursor-pointer hover:text-blue-custom">
                     <BsBoxArrowUpRight size={20}/> Live project
                 </a>
-                <a href={p.githubLink} target="_blank" className="gap-2 flex items-center cursor-pointer hover:text-blue-custom">
+                <a href={project.githubLink} target="_blank" className="gap-2 flex items-center cursor-pointer hover:text-blue-custom">
                     <BsGithub size={20}/> Github
                 </a>
             </div>
             <img
-              src={p.img}
-              alt={p.title}
+              src={project.img}
+              alt={project.title}
               className="h-full w-full object-cover transition duration-500 rounded-xl filter group-hover:scale-105 group-hover:brightness-30"
             />
           </div>
 
           <div className="flex flex-1 flex-col py-4">
-            <h3 className="font-semibold text-lg text-white">{p.title}</h3>
-            <p className="mt-1 flex-1 text-sm text-white/70">{p.description}</p>
+            <h3 className="font-semibold text-lg text-white">{project.title}</h3>
+            <p className="mt-1 flex-1 text-sm text-white/70">{project.description}</p>
 
             <div className="mt-4 border-t border-white/10 pt-3 text-xs text-white/50 ">
               <p className="flex gap-2 mb-1">
-                Built with: {p.builtWith.map((tech) => tech)}
+                Built with: {project.builtWith.map((tech) => tech)}
               </p>
             </div>
           </div>
